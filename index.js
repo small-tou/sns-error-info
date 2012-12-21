73e39e96ceab3a80505d07f4aa408052e1df1d03
tianqi <xinyu198736@gmail.com>
2012-12-21 16:45:02 +0800

    hello

---
+++ index.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  "sina":{
+    "10001":{
+      en:"System error",
+      cn:"系统错误"
+    }
+  },
+  "renren":{
+    
+  },
+  "douban":{
+    
+  },
+  "tqq":{
+    
+  },
+  "taobao":{
+    
+  }
+}
