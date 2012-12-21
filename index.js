0284fd102e2abfc11deb84515be007726153cbca
tianqi <xinyu198736@gmail.com>
2012-12-21 16:52:25 +0800

    添加doc

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 
 module.exports = {
   "sina":{
+    "doc":"http://open.weibo.com/wiki/Error_code",
     "10001":{
       en:"System error",
       cn:"系统错误"
     }
   },
   "renren":{
-    
+    "doc":"http://wiki.dev.renren.com/wiki/API_Error_Code"
   },
   "douban":{
-    
+    "doc":"http://developers.douban.com/wiki/?title=api_v2"
   },
   "tqq":{
-    
+    "doc":"http://wiki.open.t.qq.com/index.php/API%E6%96%87%E6%A1%A3/%E9%94%99%E8%AF%AF%E7%A0%81%E8%AF%B4%E6%98%8E"
   },
   "taobao":{
-    
+    "doc":"http://open.taobao.com/doc/detail.htm?spm=0.0.0.31.KZd3yV&id=114"
   }
 }
